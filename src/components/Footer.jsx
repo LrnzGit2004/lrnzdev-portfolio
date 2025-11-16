7e3b14540010b1b92233a1013cf1f9eb55d1391b
Eric Lorryl SOBZE <134990909+LrnzGit2004@users.noreply.github.com>
2025-11-16 22:21:31 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,7 @@ const Footer = () => {
     <footer className="w-full mt-20 border-t border-[#1F2937]!">
       <div className="max-w-5xl mx-auto px-4 py-6 text-center">
         <p className="text-sm text-slate-600">
-          © {year}. Made with <span className="text-red-500">❤</span> by{" "}
-          <span className="font-semibold text-slate-700">
-            Ing Eric Lorryl SOBZE
-          </span>
-          . All rights reserved.
+          © {year}.Crafting digital experiences with passion • Ing Eric Lorryl SOBZE"
         </p>
       </div>
     </footer>
